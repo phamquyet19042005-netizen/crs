f884ae01dacad3c20a29ad99ac1ff2dc33cfd6e6
phamquyet19042005-netizen <phamquyet19042005@gmail.com>
2026-08-10 10:04:43 +0700

    feat(course-service): search/pagination + internal reserve/release seat API

---
+++ course-service/src/main/java/vn/edu/crs/course_service/entity/Course.java
@@ -0,0 +1,30 @@
+package vn.edu.crs.course_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "course")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tenMonHoc;
+
+    private Integer soTinChi;
+
+    private Integer soChoToiDa;
+
+    private Integer soChoConLai;
+}
